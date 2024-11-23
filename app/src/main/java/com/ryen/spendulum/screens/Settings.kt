de37416feae7725f6e6852712601bca3009da3a7
Sarfraz Ryen <mdsarfraz.ilanos1915@gmail.com>
2024-11-23 22:30:26 +0530

    minor ui changes (settings screen)

--- app/src/main/java/com/ryen/spendulum/screens/Settings.kt
+++ app/src/main/java/com/ryen/spendulum/screens/Settings.kt
@@ -4,13 +4,8 @@ package com.ryen.spendulum.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MediumTopAppBar
